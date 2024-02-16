91cba74042139d2c9c38aef11ca9d633d7663448
ghozali25 <ghozali.ahmd25@gmail.com>
2024-02-16 11:17:09 +0700

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="c-footer">
+    <div>Copyright © {{ date('Y') }}<br>Developed by <strong>Ahmad Ghozali</strong></div>
+
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.0</strong></div>
+</footer>
